1d6ef6f721f71f0768a85def450c082e42193c66
Ricardo <ricardo@conceitho.com>
2024-05-22 23:24:17 -0300

    feat: adiciona servidor http

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,29 +5,41 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
 
 	"github.com/rgoncalvesrr/fullcyle-client-server-api/internal/core/entity"
 )
 
-type resp struct {
+type DataBody struct {
 	Data entity.Cotacao `json:"USDBRL"`
 }
 
-func main() {
+func BuscaCotacaoHandler(w http.ResponseWriter, r *http.Request) {
 	req, err := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Erro ao fazer a requisição %v", err)
+		fmt.Fprintf(w, "Erro ao fazer a requisição %v", err)
 	}
 	defer req.Body.Close()
 	_body, err := io.ReadAll(req.Body)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Erro ao ler stream %v", err)
+		fmt.Fprintf(w, "Erro ao ler stream %v", err)
 	}
-	var r resp
+	var _dataBody DataBody
 
-	if err = json.Unmarshal(_body, &r); err != nil {
-		fmt.Fprintf(os.Stderr, "Erro ao fazer decodificar json %v", err)
+	if err = json.Unmarshal(_body, &_dataBody); err != nil {
+		fmt.Fprintf(w, "Erro ao fazer decodificar json %v", err)
 	}
-	fmt.Printf("%+v", r.Data)
+
+	_json, err := json.Marshal(_dataBody.Data)
+	if err != nil {
+		fmt.Fprintf(w, "Erro codificar json %v", err)
+	}
+
+	w.Write(_json)
+
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", BuscaCotacaoHandler)
+	http.ListenAndServe(":3000", mux)
 }
